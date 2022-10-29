67444d738a600655876432bde7edf92406d3911b
AlvinBirley <alvinbirley@gmail.com>
2022-10-28 21:26:03 -0500

    Add hover color change to grid cells

--- java.js
+++ java.js
@@ -7,8 +7,12 @@ function makeRows(rows, cols) {
     for (i = 0; i < (rows * cols); i ++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
         }
+        )
     };
+};
 
 makeRows(16, 16);
 
@@ -16,3 +20,5 @@ function promptMe() {
     let promptUser = prompt("Choose Etch-A-Sketch Size");
 }
 
+let color = ["#3C9EE7", "#E7993C",
+"#E73C99", "#3CE746", "#E7993C"];
