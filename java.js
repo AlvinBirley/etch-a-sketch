ebe86a821e9826f3d0bd70835888f84c120d64dc
AlvinBirley <alvinbirley@gmail.com>
2022-11-02 08:32:16 -0500

    Make updates

--- java.js
+++ java.js
@@ -1,24 +1,48 @@
-const container = document.querySelector('#container');
-let cell = document.createElement("div");
+document.addEventListener("DOMContentLoaded", function(){
+    createBoard(16);
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    }
+    )
+    console.log("check");
+})
 
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (i = 0; i < (rows * cols); i ++) {
-        let cell = document.createElement("div");
-        container.appendChild(cell).className = "grid-item";
-        cell.addEventListener("mouseover", () => {
-            cell.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
-        }
-        )
-    };
-};
+function createBoard(size){
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-makeRows(16, 16);
+    let numDivs = size * size;
 
-function promptMe() {
-    let promptUser = prompt("Choose Etch-A-Sketch Size");
+
+    for (let i =0; i < numDivs; i++) {
+        let div = document.createElement("div");
+      div.addEventListener("mouseover", function() {
+        div.style.backgroundColor="black";
+      })
+      
+        board.insertAdjacentElement("beforeend", div);
+        
+    }
 }
 
-let color = ["#3C9EE7", "#E7993C",
-"#E73C99", "#3CE746", "#E7993C"];
+function getSize(){
+    let input = prompt("What will be the size of the board")
+    let message =document.querySelector("#message")
+    if (input == ""){
+        message.innerHTML = "Please provide a number"
+    }
+    else if(input < 0 || input > 100){
+    message.innerHTML = "Provide a number between 1 and 100"
+    }
+    else {
+        message.innerHTML = "Now you play!"
+    }
+  return input;
+}
+
+function reset (){
+    location.reload();
+}
